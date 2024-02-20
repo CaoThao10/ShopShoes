0d867f2a2db36d083609ca8c20940660d89c95ab
caothao10 <caothithao10022003@gmail.com>
2024-02-20 23:49:38 +0700

    up

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+// import Navbar from "../components/navbar/Navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default HomePage;
