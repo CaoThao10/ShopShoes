993139302b8f4cad18a33b609961562baaee0c4a
caothao10 <caothithao10022003@gmail.com>
2024-02-23 14:02:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./pages/UserDashboard/login/Login";
+import Register from "./pages/UserDashboard/register/Register";
+import Cart from "./pages/UserDashboard/cart/Cart";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </>
