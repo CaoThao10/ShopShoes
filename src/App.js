a0d58ec35120996f27d1f2b10a647fc72d72a990
caothao10 <caothithao10022003@gmail.com>
2024-02-29 13:06:54 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/UserDashboard/login/Login";
@@ -12,8 +13,27 @@ import SaleProduct from "./components/Sale/SaleProduct";
 import Accessory from "./components/accessory/Accessory";
 import Contact from "./components/contact/Contact";
 import AdminLayout from "./pages/AdminDashboard/AdminLayout";
+import Home from "./pages/AdminDashboard/Home";
+import ManageCustomer from "./pages/AdminDashboard/ManageCustomer";
+import ManageOrder from "./pages/AdminDashboard/ManageOrder";
+import ManageProduct from "./pages/AdminDashboard/ManageProduct";
+import CreateProduct from "./pages/AdminDashboard/CreateProduct";
+import ManageUser from "./pages/AdminDashboard/ManageUser";
+import Supplier from "./pages/AdminDashboard/Supplier";
+import ManageNotification from "./pages/AdminDashboard/ManageNotification";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    // Kiểm tra xem có thông tin người dùng trong Local Storage không
+    const storedUser = localStorage.getItem("user");
+
+    // Nếu có, chuyển đổi chuỗi JSON thành đối tượng và cập nhật state
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
   return (
     <>
       <BrowserRouter>
@@ -32,17 +52,28 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Route path="/admin" element={<AdminLayout />}>
-          {/* <Route path="home" element={<HomeProduct />} /> */}
-          {/* <Route path="manage-user" element={<ManageCustomer />} />
+          <Route path="home" element={<Home />} />
+          <Route path="manage-user" element={<ManageCustomer />} />
           <Route path="manage-order" element={<ManageOrder />} />
           <Route path="manage-product" element={<ManageProduct />} />
           <Route path="create-product" element={<CreateProduct />} />
           <Route path="edit-product/:id" element={<CreateProduct />} />
           <Route path="manage-customer" element={<ManageUser />} />
           <Route path="supplier" element={<Supplier />} />
-          <Route path="manage-notification" element={<ManageNotification />} /> */}
+          <Route path="manage-notification" element={<ManageNotification />} />
         </Route>
       </BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 }
